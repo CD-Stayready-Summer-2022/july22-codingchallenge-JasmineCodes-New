5304a5852ffe1fc56ccc7579069725dcdeacee7b
JasmineCodes-New <jasmine.irvin@ymail.com>
2022-07-22 10:31:58 -0400

    attempt

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -12,12 +12,23 @@ public class Solution {
      * 4 is equal to 4
      * 4 is 1 away from 5
      * while
-     * 1 is not included because the closest releation to 1 is 3 and that's 2 away.
+     * 1 is not included because the closest relation to 1 is 3 and that's 2 away.
      * @param number
      * @param possibleFamilyMembers
      * @return
      */
     public Integer[] numberFamily(Integer number, Integer[] possibleFamilyMembers){
+        for (int i = 0; i < possibleFamilyMembers.length; i++) {
+            // if i = number + 1, store in new array
+            if (possibleFamilyMembers[i] == number++) {
+                //return within 1 of family member / integer number
+                return new Integer[] {};
+            }
+            // else, do not return number in new array
+            else {
+            }
+        }
+        // return new array of integers
         return null;
     }
 }
